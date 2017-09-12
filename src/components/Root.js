5589bc67e6cd222e40a516f2642437c866c6093d
Kai Davenport <kaiyadavenport@gmail.com>
2017-09-12 13:49:50 +0100

    stuff

---
+++ src/components/Root.js
@@ -0,0 +1,11 @@
+import React, { Component, PropTypes } from 'react'
+
+class Root extends Component {
+  render() {
+    return (
+      <div>Hello</div>
+    )
+  }
+}
+
+export default Root
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + '/dist',
+    filename: "index.js"
+  },
+  module: {
+      
+  }
+};
